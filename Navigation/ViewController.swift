693e9fe1a3714f9558c1608c4412a8ade9171cd8
27ugeen <27ugeen@gmail.com>
2021-10-17 22:11:49 +0300

    added start project

--- Navigation/ViewController.swift
+++ Navigation/ViewController.swift
@@ -2,18 +2,14 @@
 //  ViewController.swift
 //  Navigation
 //
-//  Created by GiN Eugene on 17/10/21.
+//  Created by GiN Eugene on 19.07.2021.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
